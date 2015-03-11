fb865e9c6fd30eb131d5c197148d1d78398e53a5
kveloria3 <klveloria@yahoo.com>
2015-03-11 06:28:33 -0700

    Update DeckTester.java

--- src/DeckTester.java
+++ src/DeckTester.java
@@ -3,11 +3,15 @@
  */
 public class DeckTester {
 
-	/**
-	 * The main method in this class checks the Deck operations for consistency.
-	 *	@param args is not used.
-	 */
-	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
-	}
+ /**
+  * The main method in this class checks the Deck operations for consistency.
+  * @param args is not used.
+  */
+ public static void main(String[] args) {
+   String[] ranks = {"A", "B", "C"};
+   String[] suits = {"Giraffles", "Lions"};
+   int[] values = {2, 1, 6};
+   Deck deck = new Deck(ranks, suits, values);
+   System.out.println(deck);
+ }
 }
